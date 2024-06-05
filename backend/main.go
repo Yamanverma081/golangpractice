1f5f22d65873c4bccadb5f0f42a4a3a932944c5e
mac <mac@MacBook-Pro.local>
2024-06-05 10:43:53 +0530

    Batch insert method for define Batch size

--- backend/main.go
+++ backend/main.go
@@ -26,18 +26,23 @@ func main() {
 
 	dsn := "root:yaman@tcp(localhost:3333)/daily_diary?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{CreateBatchSize: 1000})
+	db := db.Session(&gorm.Session{CreateBatchSize: 1000})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
 	db.AutoMigrate(&Users{})
 
-	users := []*Users{
-		{Firstname: "yaman", Lastname: "verma"},
-		{Firstname: "kunal", Lastname: "verma"},
+	var users = [5000]Users{
+		{Firstname: "yaman", Lastname: "verma", Username: "yaman123", Email: "yamanverma123@gmail.com", Password: "yaman123", Confirmpassword: "yaman123", Mobilenumber: "7691092717"},
+		{Firstname: "kunal", Lastname: "verma", Username: "kunal123", Email: "kunalverma123@gmail.com", Password: "yaman123", Confirmpassword: "yaman123", Mobilenumber: "9509615887"},
 	}
 	result := db.Create(users)
+
+	for _, user := range users {
+		log.Printf("User ID: %d", user.ID)
+	}
 
 	result.RowsAffected = db.RowsAffected
 
